35ab245071706b483c33d515e860f82a4b8165d0
KwonSunJae <ksun4131@gmail.com>
2023-08-16 01:55:28 +0900

    feat : main view enroll modal complete

---
+++ client/src/components/profileModal/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const Profile = () => {
+  const [profileImg, setProfileImg] = useState('');
+  const [readmeURL, setReadmeURL] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 여기서 데이터를 서버로 보내는 작업을 수행할 수 있습니다.
+    console.log('Submitted Data:', {
+      profileImg,
+      readmeURL,
+      title,
+      description,
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1>Upload Project</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="profileImg">Profile Image URL:</label>
+        <input
+          type="text"
+          id="profileImg"
+          value={profileImg}
+          onChange={(e) => setProfileImg(e.target.value)}
+          required
+        />
+
+        <label htmlFor="readmeURL">github Readme.md URL:</label>
+        <input
+          type="text"
+          id="readmeURL"
+          value={readmeURL}
+          onChange={(e) => setReadmeURL(e.target.value)}
+          required
+        />
+
+        <label htmlFor="title">프로젝트 명:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+
+        <label htmlFor="description">설명:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
