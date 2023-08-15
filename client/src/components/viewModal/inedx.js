f8687d9ed19148fa4f44f8cd82ee4ccd1c8d8e14
KwonSunJae <ksun4131@gmail.com>
2023-08-16 07:46:00 +0900

    feat : view render readme and like and reaple

--- client/src/components/viewModal/inedx.js
+++ client/src/components/viewModal/inedx.js
@@ -1,56 +1,85 @@
-import React, { useState } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './index.css';
+import remarkGfm from 'remark-gfm'
+import MDEditor from '@uiw/react-md-editor';
+const View = ({ url }) => {
+    const [readmeURL, setReadmeURL] = useState(url);
+    const [markdownContent, setMarkdownContent] = useState('');
+    const [comment, setComment] = useState('');
+    const [comments, setComments] = useState([]);
+    const [liked, setLiked] = useState(false);
 
-const ExtractedContent = ({ content }) => {
-    return (
-        <div className="extracted-content" dangerouslySetInnerHTML={{ __html: content }} />
-    );
-};
-
-const App = () => {
-    const [readmeURL, setReadmeURL] = useState('');
-    const [extractedContent, setExtractedContent] = useState('');
-
-    const handleURLChange = (e) => {
-        setReadmeURL(e.target.value);
+    const handleLikeClick = () => {
+        setLiked(!liked);
     };
 
+    const handleCommentChange = (e) => {
+        setComment(e.target.value);
+    };
+
+    const handleCommentSubmit = () => {
+        if (comment.trim() !== '') {
+            setComments([...comments, comment]);
+            setComment('');
+        }
+    };
+
+    useEffect(() => {
+        // 여기에 실행하고자 하는 함수를 작성합니다.
+        handleFetchClick();
+
+        // 필요에 따라 정리(clean-up) 함수를 반환할 수 있습니다.
+        // 이 함수는 컴포넌트가 unmount될 때 실행됩니다.
+        return () => {
+            console.log('Component will unmount!');
+        };
+    }, []);
+
     const handleFetchClick = () => {
-        axios.get(readmeURL)
-            .then((response) => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(response.data, 'text/html');
-                const targetDiv = doc.querySelector('#target-div'); // Change to your target div selector
-                if (targetDiv) {
-                    setExtractedContent(targetDiv.innerHTML);
-                } else {
-                    setExtractedContent('Target div not found.');
-                }
+        fetch(readmeURL)
+            .then((response) => response.text())
+            .then((data) => {
+                console.log(data);
+                setMarkdownContent(data);
             })
             .catch((error) => {
-                console.error('Error fetching content:', error);
-                setExtractedContent('Error fetching content.');
+                console.error('Error fetching readme:', error);
+                setMarkdownContent('Error fetching readme.');
             });
     };
 
     return (
         <div className="container">
-            <h1>Extract Content</h1>
-            <div className="input-container">
-                <input
-                    type="text"
-                    placeholder="Enter URL"
-                    value={readmeURL}
-                    onChange={handleURLChange}
+            <h1>Render README</h1>
+            <div className="markdown-container" >
+                {/* <ReactMarkdown remarkPlugins={[remarkGfm]} ></ReactMarkdown> */}
+                <MDEditor.Markdown
+                    style={{ padding: 10 }}
+                    source={markdownContent}
                 />
-                <button onClick={handleFetchClick}>Fetch</button>
             </div>
-            <div className="extracted-container">
-                <ExtractedContent content={extractedContent} />
+            <div className="like-container">
+                <button onClick={handleLikeClick}>
+                    {liked ? 'Unlike' : 'Like'}
+                </button>
+                <span>{liked ? 'Liked!' : 'Not Liked'}</span>
+            </div>
+            <div className="comment-container">
+                <textarea
+                    value={comment}
+                    onChange={handleCommentChange}
+                    placeholder="Write a comment..."
+                />
+                <button onClick={handleCommentSubmit}>Submit</button>
+                <div className="comments">
+                    {comments.map((c, index) => (
+                        <div key={index}>{c}</div>
+                    ))}
+                </div>
             </div>
         </div>
     );
 };
 
-export default App;
+export default View;
