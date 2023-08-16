4340686af11a85ed322def4b6386a7a77d13d176
kcs19 <changsung19@gmail.com>
2023-08-17 04:11:46 +0900

    feat : dislike islike getAuth updateTeam getmain

--- server/controllers/team_ctrl.js
+++ server/controllers/team_ctrl.js
@@ -4,6 +4,22 @@ const reaple = require("../models/reaple");
 const logger = require("../modules/winton");
 
 const team = {
+    getMain: async (req, res, next) => {
+
+        const allTeams = await team.findAll({
+            attributes: ["name", "title", "profileImg", "uuid"],
+        });
+
+        // teams 배열 생성
+        const teams = allTeams.map(team => ({
+            name: team.name,
+            title: team.title,
+            profileImg: team.profileImg,
+            uuid: team.uuid,
+        }));
+        return res.status(200).json({ teams: teams });
+    },
+
     getTeam: async (req, res, next) => {
         let teamUuid = req.params.team_uuid;
         if (!teamUuid) {
@@ -48,6 +64,33 @@ const team = {
     },
 
 
+    isLikeAvailable: async (req, res, next) => {
+        const teamUuid = req.params.uuid;
+        if (!teamUuid) {
+            res.json({ results: false, message: "No request params" });
+            return;
+        }
+
+        const like_team_list = await team.findOne({ //heart한 팀들 가져오기
+            attributes: ["heart"],
+            where: {
+                uuid: team_uuid,
+            },
+        });
+
+        const is_team_name = 0;
+        if (like_team_list.includes(like_team_name)) {  //좋아요한 팀 존재 체크
+            console.log("Team name already exists in heart");
+            is_team_name = 1;
+        }
+
+        if (like_team_list.length >= 10 || is_team_name) {
+            return res.status(400).json({ isAvailable: false });
+        } else {
+            return res.status(200).json({ isAvailable: true });
+        }
+
+    };
 };
 
 
@@ -104,27 +147,81 @@ const process = {
 
         const filtered_team_list = like_team_list.filter(teamName => teamName !== dislike_team_name);
 
-        if (like_team_list.length === filteredHeart.length) {
+        if (like_team_list.length === filtered_team_list.length) { //제외된 팀이 없으면 dislike 불가
             console.log("Team not found in heart");
             return res.status(404).json({ message: "Team not found in heart" });
-        }
-
-        if (like_team_list.length >= 10 || is_team_name) {
-            return res.status(400).json({ error: "좋아요 불가" });
         } else {
-            like_team_list.push(like_team_name);
             db.team
                 .update({
-                    heart: JSON.stringify(like_team_list),
+                    heart: JSON.stringify(filtered_team_list),
                 },
                     {
                         where: { uuid: team_uuid, },
-                    }).catch((error) => { console.log(error) });
+                    })
+                .catch((error) => { console.log(error) });
         }
 
     },
-};
 
+    getAuth: async (req, res, next) => {
+        let likeTeam_info = req.body;
+        logger.info(JSON.stringify(req.body));
+
+        const name = req.body.name;
+        const pw = req.body.pw;
+
+        const is_team = await team.findOne({ //heart한 팀들 가져오기
+            attributes: ["uuid"],
+            where: {
+                name: name,
+                pw: pw,
+            },
+        });
+
+        if (is_team) { //제외된 팀이 없으면 dislike 불가
+            console.log("login");
+            return res.status(200).json({ uuid: is_team });
+        } else {
+            console.log("fail");
+            return res.status(404).json({ uuid: "error" });
+        }
+    },
+
+    updateTeam: async (req, res, next) => {
+        let likeTeam_info = req.body;
+        logger.info(JSON.stringify(req.body));
+
+        const uuid = req.body.uuid;
+        const profileImg = req.body.profileImg;
+        const readmeURL = req.body.readmeURL;
+        const title = req.body.title;
+        const description = req.body.description;
+
+        const foundTeam = await team.findOne({ //heart한 팀들 가져오기
+            where: {
+                uuid: uuid,
+            },
+        });
+
+        if (foundTeam) {
+            // 팀 정보 업데이트
+            await foundTeam.update({
+                profileImg: profileImg,
+                readmeURL: readmeURL,
+                title: title,
+                description: description,
+            });
+
+            console.log("Team information updated successfully");
+            return res.status(200).json({ message: "Team information updated successfully" });
+        } else {
+            console.log("Team not found");
+            return res.status(404).json({ error: "Team not found" });
+        }
+    };
+
+
+}
 
 
 module.exports = {
